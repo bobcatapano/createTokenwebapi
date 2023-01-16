8a25be8d30d97b35f804eeacc6951ff6d8d754b4
Rob Catapano <rob.catapano@gmail.com>
2023-01-15 21:44:21 -0700

    first checkin

---
+++ webapi2/webapi2/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Security.Claims;
+using webapi2.Models;
+
+namespace webapi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //For admin Only
+        [HttpGet]
+        [Route("Admins")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminEndPoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"Hi you are an {currentUser.Role}");
+        }
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                if (userClaims != null)
+                {
+                    return new UserModel
+                    {
+                        Username = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value,
+                        Role = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value
+                    };
+                }
+            }
+#pragma warning disable CS8603 // Possible null reference return.
+            return null;
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+    }
+}
---
+++ webapi2/webapi2/Models/UserConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace webapi2.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new()
+            {
+                    new UserModel(){ Username="naeem",Password="naeem_admin",Role="Admin"}
+            };
+    }
+}
